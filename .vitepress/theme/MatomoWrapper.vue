308e3deedd577ea745616930d7aed77de479703f
Djyp <djyp@plus.yt>
2025-02-02 23:16:26 +0100

    Adding tracker as script

--- .vitepress/theme/MatomoWrapper.vue
+++ .vitepress/theme/MatomoWrapper.vue
@@ -1,15 +1,26 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 const { Layout } = DefaultTheme
-window._paq.push(['trackPageView']); //To track pageview
 </script>
 
 <template>
   <Layout>
     <template #layout-top>
-        <!-- Matomo Image Tracker-->
-        <img referrerpolicy="no-referrer-when-downgrade" src="https://matomo.plus.yt/matomo.php?idsite=2&amp;rec=1" style="border:0" alt="" />
-        <!-- End Matomo -->
+        <!-- Matomo -->
+        <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//matomo.plus.yt/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+        </script>
+        <!-- End Matomo Code -->
     </template>
   </Layout>
 </template>
