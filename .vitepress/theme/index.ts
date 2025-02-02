60891899156a78466ab7f2044e214906b279bdff
Djyp <djyp@plus.yt>
2025-02-02 23:56:10 +0100

    Putting matomo directly in build

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,37 +1,33 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue';
-import type { Theme } from 'vitepress';
-import DefaultTheme from 'vitepress/theme';
-import MatomoWrapper from './MatomoWrapper.vue';
+import { h, onMounted } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
 import "@catppuccin/vitepress/theme/mocha/yellow.css";
 // import './style.css'
 
 export default {
   extends: DefaultTheme,
-  // Layout: MatomoWrapper,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  setup() {
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  },
+  setup(app) {
     onMounted(() => {
-      if (typeof window !== "undefined") {
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="//matomo.plus.yt/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '2']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-      }
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+          var u="//matomo.plus.yt/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '2']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
     })
   }
-  // enhanceApp({ app, router, siteData }) {
-  //   app.component('MatomoWrapper', MatomoWrapper)
-  // },
 } satisfies Theme
