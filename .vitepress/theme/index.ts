80a1e2b46b453196e09a53ad18f2e325fc6fdcdc
Djyp <djyp@plus.yt>
2025-02-02 23:47:23 +0100

    Putting matomo directly in build

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,13 +8,30 @@ import "@catppuccin/vitepress/theme/mocha/yellow.css";
 
 export default {
   extends: DefaultTheme,
-  Layout: MatomoWrapper,
-  // Layout: () => {
-  //   return h(DefaultTheme.Layout, null, {
-  //     // https://vitepress.dev/guide/extending-default-theme#layout-slots
-  //   })
-  // },
-  enhanceApp({ app, router, siteData }) {
-    app.component('MatomoWrapper', MatomoWrapper)
+  // Layout: MatomoWrapper,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
   },
+  setup() {
+    onMounted(() => {
+      if (typeof window !== "undefined") {
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//matomo.plus.yt/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      }
+    })
+  }
+  // enhanceApp({ app, router, siteData }) {
+  //   app.component('MatomoWrapper', MatomoWrapper)
+  // },
 } satisfies Theme
